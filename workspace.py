21e792e84e1a18e8d1d4617f3f0b2bc940b45dfa
Igor A <autin.igor@gmail.com>
2019-07-29 12:16:01 -0600

    Added buttons onto screen.
    Added start + quit buttons onto screen.

--- workspace.py
+++ workspace.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, os
 
 print("launching pygame")
 pygame.init()
@@ -14,13 +14,44 @@ screen = pygame.display.set_mode([screen_width,screen_height])
 running = True
 time = 0
 background = pygame.image.load("baba_yaga/imgs/menus/main_menu/baba_yaga_menu_background.png")
+
+current_page = "menu"
+items_on_screen = []
 while (running == True):
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.mouse.get_pos()
+            
+    if current_page == "menu":
+
+        start_button = {
+        "name" : "start_button", 
+        "x_pos" : 500,
+        "y_pos" : 600,
+        "file_location" : "baba_yaga/imgs/menus/main_menu/start_unpressed.png"
+        }
+        quit_button = {
+        "name" : "quit_button", 
+        "x_pos" : 900,
+        "y_pos" : 600,
+        "file_location" : "baba_yaga/imgs/menus/main_menu/end_unpressed.png"
+        }
+
+        items_on_screen.append(start_button)
+        items_on_screen.append(quit_button)
+
+
 
     screen.blit(background,(0,0))
+
+    for item in items_on_screen:
+
+        item_surface = pygame.image.load(item["file_location"])
+        screen.blit(item_surface, (item["x_pos"],item["y_pos"]))
+
     pygame.display.flip()
 
 program_clock.tick(60)
