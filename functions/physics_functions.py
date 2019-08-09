7b05f35632ec5f47dacdfcbf1f81f2af29be0798
Igor A <autin.igor@gmail.com>
2019-08-09 15:24:41 -0600

    Added acceleration
    added some sort of acceleration

--- functions/physics_functions.py
+++ functions/physics_functions.py
@@ -8,8 +8,19 @@ def physics(screen, items_on_screen, screen_dimensions, program_clock):
     vertical_velocity = 0
     horizontal_acceleration = 0
     vertical_acceleration = 0
+    #item_on_screen
     #[SQLID, Name, Inital Location X, Inital Location y, width, height, button alternate 1, butotn alternate 2]
     #velocity = vel init + accel * time
+    #screen_dimesions = {
+    # "screen_width" : width,
+    #  "screen_height" : height,
+    #  "width_unit" : width_unit,
+    #  "height_unit" : height_unit,
+    #  "width_in_units": width_in_units,
+    #  "height_in_units" : height_in_units}
+
+    #keys
+    #a = 97 w = 119 s = 115 d = 100
 
     while running == True:
 
@@ -17,22 +28,31 @@ def physics(screen, items_on_screen, screen_dimensions, program_clock):
             if event.type == pygame.QUIT:
                 running = False
 
-            #pygame.key.set_repeat(1,10)
-            
+
             if event.type == pygame.KEYDOWN:
                 pressed_keys = pygame.key.get_pressed()
-                #pygame.key.name()
-                print(pressed_keys)
-
-        for item in items_on_screen:
-            item_surface = pygame.transform.scale(pygame.image.load(item[8]),(item[4]*screen_dimensions["width_unit"],item[5]*screen_dimensions["height_unit"]))
-            screen.blit(item_surface, (item[2]*screen_dimensions["width_unit"],item[3]*screen_dimensions["height_unit"]))
+                if pressed_keys[97] == 1:
+                    print("a")
+                    horizontal_acceleration = horizontal_acceleration - 3
+                if pressed_keys[119] == 1:
+                    print("w")
+                    vertical_acceleration = vertical_acceleration - 3
+                if pressed_keys[115] == 1:
+                    print("s")
+                    vertical_acceleration = vertical_acceleration + 3
+                if pressed_keys[100] == 1:
+                    print("d")
+                    horizontal_acceleration = horizontal_acceleration + 3
         
-        if items_on_screen[0][2] < 200:
-            items_on_screen[0][2] += 1
+        #updates all items on the screen each loop
+        for item in items_on_screen:
+            if item[1] == 'start':
+                item_surface = pygame.transform.scale(pygame.image.load(item[8]),(item[4]*screen_dimensions["width_unit"],item[5]*screen_dimensions["height_unit"]))
+                screen.blit(item_surface, (item[2]*screen_dimensions["width_unit"]+horizontal_velocity,item[3]*screen_dimensions["height_unit"]+vertical_velocity))
+            else:
+                item_surface = pygame.transform.scale(pygame.image.load(item[8]),(item[4]*screen_dimensions["width_unit"],item[5]*screen_dimensions["height_unit"]))
+                screen.blit(item_surface, (item[2]*screen_dimensions["width_unit"],item[3]*screen_dimensions["height_unit"]))
 
         pygame.display.flip()
-        program_clock.tick(60)
-
     pygame.display.quit()
     pygame.quit()
